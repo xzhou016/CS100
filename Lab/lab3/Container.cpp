a33bf87075ea15f43055f3a6f0c09fca5fd5d02b
xzhou016 <xzhou016@ucr.edu>
2016-04-25 14:34:17 -0700

    fix Base.cpp, container.cpp, main.cpp

--- Lab/lab3/Container.cpp
+++ Lab/lab3/Container.cpp
@@ -1,26 +1,26 @@
 classContainer{
-protected:
-  Sort*sort_function;
-public:
-  /*Constructors*/
-  Container() : sort_function(NULL){};
-  Container(Sort*function) : sort_function(function){};
+  protected:
+    Sort* sort_function;
+  public:
+    /*Constructors*/
+    Container() : sort_function(NULL){};
+    Container(Sort* function) : sort_function(function){};
 
-  /*Non Virtual Functions*/
-  voidset_sort_function(Sort*sort_function);//set the type of sorting algorithm
-  /*Pure Virtual Functions*/
-  //push the top pointer of the tree into container
-  virtualvoidadd_element(Base*element)=0;
-  //iterate through trees and output values
-  virtualvoidprint()=0;
-  //calls on the previously set sorting ­algorithm.
-  //Checks if sort_function is not null,throw exception if otherwise
-  virtualvoidsort()=0;
-  /*Essentially the only functionsneededtosort*/
-  //switchtreelocations
-  virtualvoidswap(inti,intj)=0;
-  //gettopptroftreeatindexi
-  virtualBase*at(inti)=0;
-  //returncontainersize
-  virtualintsize()=0;
+    /* Non Virtual Functions */
+    void set_sort_function(Sort*sort_function);//set the type of sorting algorithm
+    /* Pure Virtual Functions */
+    //push the top pointer of the tree into container
+    virtual void add_element( Base* element) = 0;
+    //iterate through trees and output values
+    virtual void print() = 0;
+    //calls on the previously set sorting ­algorithm.
+    //Checks if sort_function is not null,throw exception if otherwise
+    virtual void sort() = 0;
+    /*Essentially the only functionsneededtosort*/
+    //switch tree locations
+    virtual void swap(int i , int j) = 0;
+    //get topptr of tree at index i
+    virtual Base* at(int i)=0;
+    //return container size
+    virtual int size()=0;
 };
