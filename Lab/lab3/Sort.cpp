586fb7c8620045ed9dddae3fb974d5e5f06c69aa
xvill002 <xvill002@ucr.edu>
2016-04-28 11:44:11 +0000

    added sort skeletons

--- Lab/lab3/Sort.cpp
+++ Lab/lab3/Sort.cpp
@@ -7,3 +7,21 @@ class Sort{
   /* Pure Virtual Functions */
   virtual void sort( Container* container) = 0;
 };
+
+class SelectionSort : public Sort
+{
+  public:
+  SelectionSort();
+  
+  void sort(Container* container);
+
+};
+
+
+class BubbleSort : public Sort
+{
+  public:
+  BubbleSort();
+  
+  void sort(Container* container);
+};
