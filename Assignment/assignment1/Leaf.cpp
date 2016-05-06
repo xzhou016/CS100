adda6f063358b93c01de11022b55bc78f9d53738
[Xander <[xvill002@hammer.cs.ucr.edu]>
2016-05-06 11:37:13 -0700

    added Leaf.cpp

---
+++ Assignment/assignment1/Leaf.cpp
@@ -0,0 +1,51 @@
+Bin::Bin()
+{
+		command = "";
+		token = "";
+}
+
+Bin::~Bin()
+{
+		delete this;
+}
+
+void Bin::run(string command, string token)
+{
+
+}
+
+
+NotBin::NotBin()
+{
+		command = "";
+		token = "";
+}
+
+NotBin::~NotBin()
+{
+		delete this
+				
+}
+
+void NotBin::run(string command, string token)
+{
+
+}
+
+Comment::Comment()
+{
+		command = "";
+        token = "";
+}
+
+Comment::~Comment()
+{
+		delete this;
+}
+
+void Comment::run(string command, string token)
+{
+
+}
+
+
