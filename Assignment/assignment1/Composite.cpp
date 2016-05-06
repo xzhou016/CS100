58f30bf45f860c596a95e5795998da92ebb90b41
[Xander <[xvill002@hammer.cs.ucr.edu]>
2016-05-06 11:06:10 -0700

    added Composite.cpp

---
+++ Assignment/assignment1/Composite.cpp
@@ -0,0 +1,44 @@
+Executable::Executable()
+{
+		command = "";
+		token = "";
+}
+
+Executable::~Executable()
+{
+		delete this;
+}
+
+void Executable::run(string command, string token) 
+{
+
+}
+
+
+
+VarComposite::VarComposite
+{
+		PID = 1;
+}
+
+VarComposite::~Varcomposite()
+{
+		delete this;
+}
+
+void VarComposite::add(string command)
+{
+
+}
+
+void VarComposite::ANDhandler(string command)
+{
+
+}
+
+void VarComposite::ORhandler(string command);
+{
+
+}
+
+
