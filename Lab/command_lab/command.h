ffbb2593a059a0a0c5fa1bd41030dfd0ad87935a
[Xander <[xvill002@hammer.cs.ucr.edu]>
2016-05-09 15:48:31 -0700

    finished command.h

--- Lab/command_lab/command.h
+++ Lab/command_lab/command.h
@@ -19,22 +19,54 @@ class Command {
 
 class OpCommand : public Command {
 	//OpCommand Code Here
+		public:
+		OpCommand(double number)
+		{
+		   root = new Op(number);
+		};
+
+
+        
 };
 
 class AddCommand : public Command {
 	//AddCommand Code Here
+		public:
+        AddCommand(Command* CmdPtr, double number)
+		{
+				root = new Add(CmdPtr->get_root(), new Op(number));
+		};
+
 };
 
 class SubCommand : public Command {
 	//SubCommand Code Here
+	
+		public:
+        SubCommand(Command* CmdPtr, double number)
+		{
+				root = new Sub(CmdPtr->get_root(), new Op(number));
+		};
 };
 
 class MultCommand : public Command {
 	//MultCommand Code Here
+	
+		public:
+        MultCommand(Command* CmdPtr, double number)
+		{
+				root = new Mult(CmdPtr->get_root(), new Op(number));
+		};
 };
 
 class SqrCommand : public Command {
 	//SqrCommand Code Here
+	
+		public:
+        SqrCommand(Command* CmdPtr, double number)
+		{
+				root = new Sqr(CmdPtr->get_root());
+		};
 };
 
 #endif //__COMMAND_CLASS__
