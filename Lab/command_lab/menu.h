d528f54973ba3d822f0fd658504e1c41d679c3dd
xzhou016 <xzhou016@ucr.edu>
2016-05-09 15:15:00 -0700

    menu.h done

--- Lab/command_lab/menu.h
+++ Lab/command_lab/menu.h
@@ -16,24 +16,35 @@ class Menu {
 			//Base constructor to set up all necessary members
 		};
 		void execute() {
-			//Prints the current commands value (based on history_index), if no commands exist
-			//print 0 by default
+			if (history.size() != 0) {
+				cout << history.at(history_index)->execute() << endl;//Prints the current commands value (based on history_index), if no commands exist
+			}else
+				cout << 0 << endl;//print 0 by default
 		};
 		bool initialized() {
-			//Return true if the history has been primed with a single op instruciton
+			if(history_index == 0)
+				return false;
+			else
+				return true;//Return true if the history has been primed with a single op instruciton
 			//This is necessary because that is the base of a calculation
 		};
 		void add_command(Command* cmd) {
-			//Adds a command to history in the appropriate posiiton (based on history_index)
+			history.push_back(cmd);//Adds a command to history in the appropriate posiiton (based on history_index)
 		};
 		Command* get_command() {
-			//Returns the command in history we are currently referring to (based on history_index)
+			return history.at(history_index);//Returns the command in history we are currently referring to (based on history_index)
 		};
 		void undo() {
-			//Moves to the last command in history (if possible)
+			if(history_index != 0){
+				history_index--;
+				return; //Moves to the last command in history (if possible)
+			}
 		};
 		void redo() {
-			//Moves to the next command in history (if possible)
+			if(history_index != (history_index.size() - 1)){
+				history_index++;
+				return; //Moves to the next command in history (if possible)
+			}
 		};
 };
 
