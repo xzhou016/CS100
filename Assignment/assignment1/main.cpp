b20fd40829a4f13a6507efbe9154335804b396f7
xzhou016 <xzhou016@ucr.edu>
2016-05-12 22:22:24 -0700

    setup

--- Assignment/assignment1/main.cpp
+++ Assignment/assignment1/main.cpp
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string>
 
-int main(int argc, char const *argv[]) {
-  string input;
-  cout << "$ git plz:"
-  cin >> input;
-  return 0;
-}
-=======
-#include "header.h"
+
 
 
 int main(int argc, char const *argv[]) {
-  /* code */
+  
   return 0;
 }
->>>>>>> master
